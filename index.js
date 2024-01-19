5d58b58ec33c824bfb6e45eac515f221836b8f7d
Edward <joed.aguirang@gmail.com>
2024-01-20 07:23:51 +0800

    Done with Wersonal Pebsite

---
+++ index.js
@@ -0,0 +1,11 @@
+function Change2About(){
+     document.getElementById("changer").setAttribute("href","./index.css")
+}
+
+function Change2Skills(){
+     document.getElementById("changer").setAttribute("href","./skills.css")
+}
+
+function Change2Projects(){
+     document.getElementById("changer").setAttribute("href","./projects.css")
+}
